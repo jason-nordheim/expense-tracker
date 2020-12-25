b840c7acc06d7939e840099059dceefdfe26da11
jason-nordheim <jason.nordheim@gmail.com>
2020-12-25 03:39:18 -0500

    applied styling and imported doughnut

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Card, CardContent, CardHeader, Typography } from '@material-ui/core'
+import { Doughnut } from 'react-chartjs-2'
+
+import useStyles from './styles'
 
 function Details() {
+    const classes = useStyles()
+
     return (
-        <Card>
+        <Card className={classes.income}>
             <CardHeader title="Income"></CardHeader>
             <CardContent>
                 <Typography variant="h5"></Typography>
+                <Doughnut data={null}/> 
             </CardContent>
         </Card>
     )
