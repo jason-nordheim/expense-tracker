95cb3adc396da0b2d0f1aab04c4625798d813218
jason-nordheim <jason.nordheim@gmail.com>
2020-12-25 08:13:20 -0500

    added 'title' prop to 'Details' component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,22 +2,27 @@ import React from 'react'
 import Details from '../Details'
 import { Grid } from '@material-ui/core'
 
+import useStyles from '../../styles'
+
 function App() {
+    const classes = useStyles()
+
     return (
         <Grid
+            alignItems="center"
+            className={classes.grid}
             container
             spacing={0}
-            alignItems="center"
             style={{ height: '100vh' }}
         >
             <Grid item xs={12} sm={4}>
-                <Details />
+                <Details title="Income" />
             </Grid>
             <Grid item xs={12} sm={4}>
                 Main
             </Grid>
             <Grid item xs={12} sm={4}>
-                <Details />
+                <Details title="Expense" />
             </Grid>
         </Grid>
     )
