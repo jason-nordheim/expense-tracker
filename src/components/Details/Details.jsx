7bf851725222cffcbb73ac47177f912312f834f5
jason-nordheim <jason.nordheim@gmail.com>
2020-12-25 03:19:42 -0500

    rand through prettier

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Card, CardContent, CardHeader, Typography } from '@material-ui/core'
+
+function Details() {
+    return (
+        <Card>
+            <CardHeader title="Income"></CardHeader>
+            <CardContent>
+                <Typography variant="h5"></Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Details
