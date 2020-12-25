d56484bfa8f797620c60f4ca152d8bbd016f54b6
jason-nordheim <jason.nordheim@gmail.com>
2020-12-25 03:40:16 -0500

    applied grid layout to root App.tsx comonent

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Details from '../Details/Details'
+import { Grid } from '@material-ui/core'
 
 function App() {
-    return <div className="App"></div>
+    return (
+        <Grid
+            container
+            spacing={0}
+            alignItems="center"
+            style={{ height: '100vh' }}
+        >
+            <Grid item xs={12} sm={4}>
+                <Details />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                Main
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <Details />
+            </Grid>
+        </Grid>
+    )
 }
 
 export default App
